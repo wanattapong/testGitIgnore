9a75bc97b4e3a99a43d0cf9aeebb8bd352e2e380
wanattapong Ubuntu <wanattapong1997@gmail.com>
2020-07-17 14:47:09 +0700

    2migrationsData

--- my_library/book_management/models.py
+++ my_library/book_management/models.py
@@ -8,3 +8,11 @@ class Form_Diposit(models.Model):
     
     def __str__(self):
         return self.originalcode
+
+class Form_Diposiorety(models.Model):
+    originalcode = models.CharField(max_length=255, null=False, blank=True, unique=True)
+    phylum = models.CharField(max_length=255, null=True, blank=True)
+    order = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.originalcode
