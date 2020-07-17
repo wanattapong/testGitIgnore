af67e76dc5838d7a1782d3e4cc1b9bc8022a00c8
wanattapong Ubuntu <wanattapong1997@gmail.com>
2020-07-17 14:33:26 +0700

    addDatatwo

--- my_library/book_management/models.py
+++ my_library/book_management/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Form_Diposit(models.Model):
+    originalcode = models.CharField(max_length=255, null=False, blank=True, unique=True)
+    phylum = models.CharField(max_length=255, null=True, blank=True)
+    order = models.CharField(max_length=255, null=True, blank=True)
+    
+    def __str__(self):
+        return self.originalcode
